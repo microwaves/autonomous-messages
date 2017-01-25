cab77f9829497b5557ca33ccea9ae1bd52e5e351
microwaves <stephanozanzin@gmail.com>
2017-01-25 15:21:44 +0000

    Store generated public key on chrome.storage.local.

--- options.js
+++ options.js
@@ -1,5 +1,6 @@
 function generate_key_pair() {
   generateKeyPair().then(function(key) {
+    // export and store private key
     exportKey(key.privateKey).then(function(exported_private_key) {
       chrome.storage.local.set(
         { exported_private_key: exported_private_key },
@@ -7,6 +8,18 @@ function generate_key_pair() {
           var status = document.getElementById('status');
           status.textContent = 'Key pair was successfully created.';
 
+          setTimeout(function() {
+            status.textContent = '';
+          }, 750);
+        }
+      );
+    });
+
+    // export and store public key
+    exportKey(key.publicKey).then(function(exported_public_key) {
+      chrome.storage.local.set(
+        { exported_public_key: exported_public_key },
+        function() {
           setTimeout(function() {
             status.textContent = '';
           }, 750);
