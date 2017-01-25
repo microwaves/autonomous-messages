6b9c60ba0fc9ae0725cbe51fdd4b6021d7c99b85
microwaves <stephanozanzin@gmail.com>
2017-01-25 15:36:37 +0000

    Move utils to utils.js.

--- content.js
+++ content.js
@@ -41,39 +41,6 @@ function importKey(key, usage) {
 	)
 }
 
-// Decodes decrypted data to String.
-function decodeDecryptedData(arr) {
-  return new TextDecoder().decode(arr);
-}
-
-// Converts ArrayBuffer to String.
-function arrayBufferToString(buf) {
-  return String.fromCharCode.apply(null, new Uint16Array(buf));
-}
-
-// Converts String to ArrayBuffer.
-function stringToArrayBuffer(str) {
-  var buf = new ArrayBuffer(str.length*2);
-  var bufView = new Uint16Array(buf);
-
-  for (var i = 0, strLen = str.length; i < strLen; i++) {
-    bufView[i] = str.charCodeAt(i);
-  }
-
-  return buf;
-}
-
-// Generate unique IDs.
-function generateUniqueId(length) {
-  var str = "";
-  var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < length; i++)
-    str += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
-
-  return str;
-}
-
 /*
 
 Next steps:
